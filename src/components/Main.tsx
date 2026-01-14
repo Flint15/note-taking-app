e1e72fc8dc252ecfb29806ad48cebb98bc174e75
Flint15 <chesspace341@gmail.com>
2026-01-14 15:39:52 +0100

    Make textarea editable

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -1,14 +1,23 @@
+import { useState } from "react";
 import SidebarButton from "./buttons/SidebarButton";
 import "./Main.css";
 
 export default function Main({ currentNote }: { currentNote: string }) {
+  const [text, setText] = useState<string>("");
+
   return (
     <main>
       <div className="note-toolbar">
         <SidebarButton />
       </div>
       <div className="note-editor">
-        <textarea id="textarea" value={currentNote}></textarea>
+        <textarea
+          onChange={(e) => {
+            setText(e.target.value);
+          }}
+          id="textarea"
+          value={text}
+        ></textarea>
       </div>
     </main>
   );
