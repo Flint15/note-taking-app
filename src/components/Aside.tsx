c38ac1e00ca27b72efbec9f13e1bfe7675df280a
Flint15 <chesspace341@gmail.com>
2026-01-26 22:50:27 +0100

    Deleted unnecessary import

--- src/components/Aside.tsx
+++ src/components/Aside.tsx
@@ -1,4 +1,4 @@
-import { useEffect, type Dispatch, type SetStateAction } from "react";
+import { type Dispatch, type SetStateAction } from "react";
 import NewNoteButton from "./buttons/NewNoteButton";
 import "./Aside.css";
 import NoteMenuButton from "./buttons/NoteMenuButton";
